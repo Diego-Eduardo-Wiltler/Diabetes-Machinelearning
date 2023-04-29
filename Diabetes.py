c5cb5a5f32a4d6e79ce0da828f3665b523737240
Diego Eduardo Wiltler <90121713+Diego-Eduardo-Wiltler@users.noreply.github.com>
2023-04-29 10:06:11 -0300

    Add files via upload

---
+++ Diabetes.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+
+diabetes_data = pd.read_csv('D:\Thiagão Sabado\diabetes.csv')
+'''' mude o diretório de acordo'''
+
+X = diabetes_data.drop('Outcome', axis=1)
+y = diabetes_data['Outcome']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42)
+
+clf = DecisionTreeClassifier(random_state=42)
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, average='weighted')
+recall = recall_score(y_test, y_pred, average='weighted')
+f1 = f1_score(y_test, y_pred, average='weighted')
+confusion_mat = confusion_matrix(y_test, y_pred)
+
+print("Acurácia:", accuracy)
+print("Precisão:", precision)
+print("Revocação(recall):", recall)
+print("F1-SCORE:", f1)
+print("Matriz de Confusão:\n", confusion_mat)
